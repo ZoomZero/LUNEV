ad3958f8e438562146da9c4ab3d73a7c56b5bfd6
ZoomZero <zoomzerow0w@yandex.ru>
2020-10-16 02:16:51 +0300

    Not started project

---
+++ shmem_and_semaphore/main.cpp
@@ -0,0 +1,34 @@
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/sem.h>
+#include <sys/shm.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <cstring>
+
+void reader(char * filename)
+{
+  printf("reader\n");
+}
+void writer()
+{
+  printf("writer\n");
+}
+
+int main(int argc, char *argv[]) {
+  if (argc != 2 && argc != 3)
+  {
+    printf("Uncorrect number of arguments\n");
+    exit(EXIT_FAILURE);
+  }
+
+  if (strcmp(argv[1], "-r") == 0) reader(argv[2]);
+  else if (strcmp(argv[1], "-w") == 0) writer();
+  else
+  {
+    perror("WRONG ARGUMENT");
+    exit(EXIT_FAILURE);
+  }
+
+  return 0;
+}
