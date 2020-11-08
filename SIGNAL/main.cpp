79709a0656af26bec9a52b47ed0faf7fadb28b83
ZoomZero <zoomzerow0w@yandex.ru>
2020-11-08 18:43:55 +0300

    Not started project

--- SIGNAL/main.cpp
+++ SIGNAL/main.cpp
@@ -1,28 +1,23 @@
 #include <sys/types.h>
 #include <sys/ipc.h>
-#include <sys/sem.h>
-#include <sys/shm.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <cstring>
-#include "reader.h"
-#include "writer.h"
 
+int child();
+int parent();
 
-int main(int argc, char *argv[]) {
-  if (argc != 2 && argc != 3)
-  {
-    printf("Uncorrect number of arguments\n");
-    exit(EXIT_FAILURE);
-  }
+int main(int argc, char *argv[])
+{
 
-  if (strcmp(argv[1], "-r") == 0) reader(argv[2]);
-  else if (strcmp(argv[1], "-w") == 0) writer();
-  else
-  {
-    perror("WRONG ARGUMENT");
-    exit(EXIT_FAILURE);
-  }
+}
 
-  return 0;
+int child()
+{
+
+}
+
+int parent()
+{
+
 }
