a20f4c46b744f808bafe56af8e9924f65df29518
ZoomZero <zoomzerow0w@yandex.ru>
2021-02-24 15:55:46 +0300

    At least, i started it.

---
+++ data_structure/structure.h
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+typedef struct Key
+{
+
+
+} KEY;
+
+typedef struct Node
+{
+
+
+} NODE;
+
+typedef struct Tree
+{
+
+
+} TREE;
+
+typedef struct Iterator
+{
+
+
+} ITER;
+
+TREE * TreeCreate();
+int TreeSize();
+NODE  * AddNode();
+NODE * GetNode();
+NODE * TreeMin();
