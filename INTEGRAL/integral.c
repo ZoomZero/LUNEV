4ad8d154cce2b49d0c02adbf75f8093ca801ae1d
ZoomZero <zoomzerow0w@yandex.ru>
2021-04-07 15:15:18 +0300

    Fixing tabulation

--- INTEGRAL/integral.c
+++ INTEGRAL/integral.c
@@ -1,13 +1,15 @@
-#include "integral.h"
+  #include "integral.h"
 
-typedef struct ThreadData {
+typedef struct ThreadData
+{
     double a;
     double b;
     double result;
     cpu_set_t cpu_set;
 } ThreadData;
 
-typedef struct CpuInfo {
+typedef struct CpuInfo
+{
     char * hypercpu_available;
     unsigned int available_hypercpus_num;
     cpu_set_t * hypercpu_sets;
@@ -15,37 +17,37 @@ typedef struct CpuInfo {
 
 void ErrorCheck(bool err, const char * description)
 {
-	if(!err)
-	{
-		printf("%s\n", description);
-		exit(EXIT_FAILURE);
-	}
+  if(!err)
+  {
+  	printf("%s\n", description);
+  	exit(EXIT_FAILURE);
+  }
 }
 
 long string_to_int(char * str)
-{
-	long input = 0;
-	char * strptr = str;
+  {
+  long input = 0;
+  char * strptr = str;
   char * endptr = NULL;
 
-	input = strtol(strptr, &endptr, 10);
+  input = strtol(strptr, &endptr, 10);
 
-	if (endptr == strptr || *endptr != '\0')
+  if (endptr == strptr || *endptr != '\0')
   {
-		return ERROR;
-	}
+  	return ERROR;
+  }
 
-	if (errno == ERANGE && (input == LONG_MAX || input == LONG_MIN))
+  if (errno == ERANGE && (input == LONG_MAX || input == LONG_MIN))
   {
-		return ERROR;
-	}
+  	return ERROR;
+  }
 
-	return input;
+  return input;
 }
 
 int init_cpus(CpuInfo * info, unsigned int cpu_count)
 {
-	if (info == NULL)
+  if (info == NULL)
     return ERROR;
 
   info->available_hypercpus_num = get_nprocs();
@@ -58,14 +60,14 @@ int init_cpus(CpuInfo * info, unsigned int cpu_count)
 
   int cpu_fd = 0;
   int text_len = 0;
-	char text[MAX_FILE_LENGTH] = {0};
+  char text[MAX_FILE_LENGTH] = {0};
 
-	ErrorCheck((cpu_fd = open(CPU_ONLINE_FILE, O_RDONLY)), "Error opening cpu online file");
+  ErrorCheck((cpu_fd = open(CPU_ONLINE_FILE, O_RDONLY)), "Error opening cpu online file");
   ErrorCheck((text_len = read(cpu_fd, text + 1, MAX_FILE_LENGTH - 2)), "Error getting length of file");
-	ErrorCheck(close(cpu_fd) == 0, "Error closing fd");
+  ErrorCheck(close(cpu_fd) == 0, "Error closing fd");
 
-	text[0] = ',';
-	text[text_len] = '\0';
+  text[0] = ',';
+  text[text_len] = '\0';
 
   unsigned int commaIndex = 0;
   unsigned int block_start = 0;
@@ -80,7 +82,8 @@ int init_cpus(CpuInfo * info, unsigned int cpu_count)
     block_start = commaIndex;
     commaIndex++;
 
-    while (text[commaIndex] != ',' && text[commaIndex] != '\0') {
+    while (text[commaIndex] != ',' && text[commaIndex] != '\0')
+    {
       commaIndex++;
     }
 
@@ -106,7 +109,7 @@ int init_cpus(CpuInfo * info, unsigned int cpu_count)
 
   }
 
-	return SUCCESS;
+  return SUCCESS;
 }
 
 double function(double x)
@@ -116,67 +119,67 @@ double function(double x)
 
 void * calculate(void* args)
 {
-	ThreadData * data = ((ThreadData*)args);
+  ThreadData * data = ((ThreadData*)args);
 
-	cpu_set_t cpuset = ((ThreadData*)args)->cpu_set;
-	pthread_t current_thread = pthread_self();
-	double start = ((ThreadData*)args)->a;
-	double finish = ((ThreadData*)args)->b;
+  cpu_set_t cpuset = ((ThreadData*)args)->cpu_set;
+  pthread_t current_thread = pthread_self();
+  double start = ((ThreadData*)args)->a;
+  double finish = ((ThreadData*)args)->b;
 
   ErrorCheck(pthread_setaffinity_np(current_thread, sizeof(cpu_set_t), &cpuset), "Wrong affinity");
 
-	data->result = 0;
+  data->result = 0;
 
-	double delta = DX;
-	double x = start;
+  double delta = DX;
+  double x = start;
 
-	 for (; x < finish; x += delta) {
-		((ThreadData*)args)->result += function(x) * DX;
-	}
+   for (; x < finish; x += delta) {
+  	((ThreadData*)args)->result += function(x) * DX;
+  }
 
-	return NULL;
+  return NULL;
 }
 
 int cache_line_size()
 {
-	FILE * cache_info = fopen("/sys/bus/cpu/devices/cpu0/cache/index0/coherency_line_size", "r");
+  FILE * cache_info = fopen("/sys/bus/cpu/devices/cpu0/cache/index0/coherency_line_size", "r");
 
-	if (cache_info == NULL)
-		return ERROR;
+  if (cache_info == NULL)
+  	return ERROR;
 
-	int line_size = 0;
-	fscanf(cache_info, "%d", &line_size);
+  int line_size = 0;
+  fscanf(cache_info, "%d", &line_size);
 
-	fclose(cache_info);
+  fclose(cache_info);
 
-	return line_size;
+  return line_size;
 }
 
 int CreateThreadBuffers(int total, char ** data, int * buf_size)
 {
-	int line_size = 0;
-	line_size = cache_line_size();
+  int line_size = 0;
+  line_size = cache_line_size();
 
-	int info_size = sizeof(ThreadData);
+  int info_size = sizeof(ThreadData);
 
-	if (info_size <= line_size)
+  if (info_size <= line_size)
   {
-		info_size = 2 * line_size;
-	}
+  	info_size = 2 * line_size;
+  }
   else
   {
-		info_size = (info_size / line_size + 2) * line_size;
-	}
+  	info_size = (info_size / line_size + 2) * line_size;
+  }
 
-	*buf_size = info_size;
+  *buf_size = info_size;
 
-	*data = (char *) malloc(total * info_size);
+  *data = (char *) malloc(total * info_size);
 
-	if (*data == NULL)
-		return ERROR;
+  if (*data == NULL)
+  	return ERROR;
 
 
-	memset(*data, 0, total * info_size);
+  memset(*data, 0, total * info_size);
 
   return SUCCESS;
 }
@@ -187,74 +190,74 @@ double StartThreadCalc(CpuInfo * info, unsigned int cpu_count, double a, double 
     return ERROR;
 
   double begin = a;
-	double size = (double)(b - a) / (double) cpu_count;
+  double size = (double)(b - a) / (double) cpu_count;
   unsigned int max_hypercpu = MAX(cpu_count, info->available_hypercpus_num);
-	pthread_t * threads = (pthread_t *) calloc(max_hypercpu, sizeof(pthread_t));
+  pthread_t * threads = (pthread_t *) calloc(max_hypercpu, sizeof(pthread_t));
 
-	if (!threads)
-		return ERROR;
+  if (!threads)
+  	return ERROR;
 
-	int buf_size = 0;
-	char * data = NULL;
-	CreateThreadBuffers(max_hypercpu, &data, &buf_size);
+  int buf_size = 0;
+  char * data = NULL;
+  CreateThreadBuffers(max_hypercpu, &data, &buf_size);
 
-	for (unsigned i = 0; i < max_hypercpu; i++)
+  for (unsigned i = 0; i < max_hypercpu; i++)
   {
-		ThreadData * cur = (ThreadData *) (data + i * buf_size);
+  	ThreadData * cur = (ThreadData *) (data + i * buf_size);
 
-		if (i < cpu_count)
+  	if (i < cpu_count)
     {
-			*cur = (ThreadData) {
-				                    begin,
-				                    begin + size,
-				                    0,
-				                    info->hypercpu_sets[i % info->available_hypercpus_num],
+  		*cur = (ThreadData) {
+  			                    begin,
+  			                    begin + size,
+  			                    0,
+  			                    info->hypercpu_sets[i % info->available_hypercpus_num],
             		            };
     }
     else
     {
       *cur = (ThreadData) {
-				                    a,
-				                    a + (b - a) / (double)(cpu_count),
-				                    0,
-				                    info->hypercpu_sets[i % info->available_hypercpus_num],
+  			                    a,
+  			                    a + (b - a) / (double)(cpu_count),
+  			                    0,
+  			                    info->hypercpu_sets[i % info->available_hypercpus_num],
             		          };
     }
 
     begin += size;
     }
 
-	for (unsigned i = 0; i < max_hypercpu; i++)
+  for (unsigned i = 0; i < max_hypercpu; i++)
   {
-		pthread_create(&threads[i], NULL, &calculate, (data + i * buf_size));
-	}
+  	pthread_create(&threads[i], NULL, &calculate, (data + i * buf_size));
+  }
 
-	double result = 0;
-	for (unsigned i = 0; i < max_hypercpu; i++)
+  double result = 0;
+  for (unsigned i = 0; i < max_hypercpu; i++)
   {
-		pthread_join(threads[i], NULL);
-		ThreadData * cur = (ThreadData*) (data + i * buf_size);
+  	pthread_join(threads[i], NULL);
+  	ThreadData * cur = (ThreadData*) (data + i * buf_size);
 
-		if (i < cpu_count) result += cur->result;
-	}
+  	if (i < cpu_count) result += cur->result;
+  }
 
-	free(data);
+  free(data);
 
-	return result;
+  return result;
 }
 
 
 double integrate(unsigned int cpu_count, double a, double b)
 {
-	double result = 0;
-	CpuInfo * info = (CpuInfo *) calloc(1, sizeof(CpuInfo));
+  double result = 0;
+  CpuInfo * info = (CpuInfo *) calloc(1, sizeof(CpuInfo));
 
-	ErrorCheck(init_cpus(info, cpu_count) == SUCCESS, "Wrong cpu initialization");
-	result = StartThreadCalc(info, cpu_count, a, b);
+  ErrorCheck(init_cpus(info, cpu_count) == SUCCESS, "Wrong cpu initialization");
+  result = StartThreadCalc(info, cpu_count, a, b);
 
   free(info->hypercpu_available);
   free(info->hypercpu_sets);
   free(info);
 
-	return result;
+  return result;
 }
